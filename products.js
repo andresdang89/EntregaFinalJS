76f0f114e8655ed39a2c03324aa9b971bea70651
andresdang89 <andresdang89@gmail.com>
2024-02-07 16:59:20 -0500

    Agregado

---
+++ products.js
@@ -0,0 +1,23 @@
+const productos = [
+{
+    id: 1,
+    productName: "Anillo esmeralda",
+    price: 1000,
+    quanty: 1,
+    img: "./Images/anillo-esmeralda-diamantes.avif",
+},
+{
+    id: 2,
+    productName: "Aretes esmeralda",
+    price: 800,
+    quanty: 1,
+    img: "./Images/Aretes.diamantes.webp",
+},
+{
+    id: 3,
+    productName: "Pulsera esmeraldas",
+    price: 1700,
+    quanty: 1,
+    img: "./Images/white.webp",
+},
+];
